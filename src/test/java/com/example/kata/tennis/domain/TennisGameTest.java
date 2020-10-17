dc2f77d5500a7ec631310fbd854b53b2301a53aa
2020-DEV-167 <2020-DEV-167@example.com>
2020-10-17 08:47:09 +0200

    Initial commit
    * Dependencies setup
    * Check test dependencies are working

---
+++ src/test/java/com/example/kata/tennis/domain/TennisGameTest.java
@@ -0,0 +1,15 @@
+package com.example.kata.tennis.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TennisGameTest {
+
+    private TennisGame game;
+
+    @Test
+    void test() {
+        assertThat(game).isEqualTo(null);
+    }
+}
